a2d3ec8ef5ee0ff4a44b98d40185900107e56f1a
Revanthyr <155083567+Revanthyr@users.noreply.github.com>
2024-06-03 18:27:36 +0200

    Add mouseover function

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
 let gridSize = 16*16;
+let div = "";
 const body = document.querySelector("body");
 const container = document.querySelector(".container");
 for (let i = gridSize;i>0;--i){
-    
-    container.appendChild(document.createElement("div"));
+     div = document.createElement("div");
+    div.classList.add("square");
+    container.appendChild(div);
     
 }
 
+let allDivs = document.querySelectorAll(".square");
+// allDivs.forEach(callback(value,index,array));
+  
+  allDivs.forEach(item => item.addEventListener("mouseover",function(e){
+    item.classList.add("targeted");
 
+  }));
+
+   
+
+console.log(allDivs);
