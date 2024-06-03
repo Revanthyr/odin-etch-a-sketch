3ab8855119ea4f8350de1a35fc21685ae7869ef6
Revanthyr <155083567+Revanthyr@users.noreply.github.com>
2024-06-03 11:47:25 +0200

    Add square grid

---
+++ script.js
@@ -0,0 +1,10 @@
+let gridSize = 16*16;
+const body = document.querySelector("body");
+const container = document.querySelector(".container");
+for (let i = gridSize;i>0;--i){
+    
+    container.appendChild(document.createElement("div"));
+    
+}
+
+
