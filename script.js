6966e43c4a132a0e32bb6620073967fe459c8e01
Revanthyr <155083567+Revanthyr@users.noreply.github.com>
2024-06-05 16:09:18 +0200

    Add grid creation button

--- script.js
+++ script.js
@@ -2,21 +2,50 @@ let gridSize = 16*16;
 let div = "";
 const body = document.querySelector("body");
 const container = document.querySelector(".container");
-for (let i = gridSize;i>0;--i){
+
+function createGrid(gridNumber){
+for (let i = gridNumber**2;i>0;--i){
      div = document.createElement("div");
     div.classList.add("square");
+    div.style.height = `${960/(gridNumber)}px`;
+    div.style.flexBasis = `${960/(gridNumber)}px`;
     container.appendChild(div);
-    
 }
-
 let allDivs = document.querySelectorAll(".square");
-// allDivs.forEach(callback(value,index,array));
-  
   allDivs.forEach(item => item.addEventListener("mouseover",function(e){
     item.classList.add("targeted");
 
   }));
+}
+function removeGrid(){
+  container.innerHTML = "";
+}
 
+const button = document.querySelector("button");
+button.addEventListener("click",function(e){
+  let gridNumber = window.prompt("How many");
+  removeGrid();
+  createGrid(gridNumber);
+  
+console.log(allDivs);
+}) 
+
+
+  
    
 
-console.log(allDivs);
+
+
+
+/*
+ajoutert un bouttton
+evenlistener (onclick)
+popup 
+ - valeur max = 100 stocker la valeur du popup dans une variable
+remove child all divs
+executer fonction creatrice de grid 
+avec pour argument la valeur entrée
+changer la dimension des carrés selon la valeur
+
+
+*/
